ac25e06a0d7629c1df97dd5568ec3e46d3314d64
Nitroh <Nitroh.Hs@gmail.com>
2016-10-05 21:33:59 -0400

    Solution/Project creation
    Created a VisualStudio solution and project. Setup shared assembly
    information. Used Visual Studio 2015, .NET 4.6.1, with Resharper 2016
    extension.

---
+++ src/NitrohGG.Edwin/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Edwin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("NitrohGG.Edwin")]
+[assembly: Guid("efc052fc-40ee-41f4-96d0-c272d95d5646")]
